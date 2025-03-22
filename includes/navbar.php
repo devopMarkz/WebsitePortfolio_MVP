726d9dab4253d36ae8e590c1a2a8e588dc627625
devopMarkz <marcosdev2002@gmail.com>
2025-03-22 20:14:10 -0300

    Cria barra de navegacao

---
+++ includes/navbar.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<nav class="navbar">
+    <div class="nav-container">
+        <a href="users/index.php" class="nav-logo">Portfólio Digital</a>
+        <ul class="nav-links">
+            <li><a href="users/index.php">Dashboard</a></li>
+            <li><a href="users/create_portfolio.php">Criar Portfólio</a></li>
+            <li><a href="profile.php">Meu Perfil</a></li>
+            <li><a href="backend/logout.php" class="logout-btn">Sair</a></li>
+        </ul>
+    </div>
+</nav>
