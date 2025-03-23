c082396fea98a1d532391ce58ca5a7b6120daea9
devopMarkz <marcosdev2002@gmail.com>
2025-03-22 21:50:43 -0300

    V1 - Projeto

--- profile.php
+++ profile.php
@@ -22,7 +22,6 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Perfil</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/profile.css">
 </head>
 <body>
